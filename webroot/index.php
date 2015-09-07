c2f0db5fa3fc55895d1334a9b4c4b73076db3f1c
Eddie Taube <eddie.taube@gmail.com>
2015-09-07 19:40:55 +0200

    Merge branch 'master' of https://github.com/edtau/phpmvc
    # Conflicts:
    #	webroot/index.php

--- webroot/index.php
+++ webroot/index.php
@@ -1,12 +1,15 @@
 <?php
 
-
 require __DIR__.'/config_with_app.php';
+
+//To get clean url. Notice it must be before navbar to work as expected
 $app->url->setUrlType(\Anax\Url\CUrl::URL_CLEAN);
+
 
 $app->theme->configure(ANAX_APP_PATH . 'config/theme_me.php');
 $app->navbar->configure(ANAX_APP_PATH . 'config/navbar_me.php');
 
+//Routers
 
 
 
@@ -45,11 +48,6 @@ $app->router->add('me', function() use ($app) {
 
 });
 
-
-
-$app->router->add('', function() use ($app) {
-
-});
 $app->router->add('redovisning', function() use ($app) {
 
     $app->theme->setTitle("Redovisning");
@@ -66,26 +64,11 @@ $app->router->add('redovisning', function() use ($app) {
     ]);
 
 });
-$app->router->add('me', function() use ($app) {
 
-    $app->theme->setTitle("asdf");
-
-    $content = $app->fileContent->get('me.md');
-    $content = $app->textFilter->doFilter($content, 'shortcode, markdown');
-
-    $byline = $app->fileContent->get('byline.md');
-    $byline = $app->textFilter->doFilter($byline, 'shortcode, markdown');
-
-    $app->views->add('me/page', [
-        'content' => $content,
-        'byline' => $byline,
-    ]);
-
-});
 $app->router->add('source', function() use ($app) {
 
     $app->theme->addStylesheet('css/source.css');
-    $app->theme->setTitle("K�llkod");
+    $app->theme->setTitle("Källkod");
 
     $source = new \Mos\Source\CSource([
         'secure_dir' => '..',
@@ -96,11 +79,6 @@ $app->router->add('source', function() use ($app) {
     $app->views->add('me/source', [
         'content' => $source->View(),
     ]);
-
-});
-
-
-$app->router->add('source', function() use ($app) {
 
 });
 
